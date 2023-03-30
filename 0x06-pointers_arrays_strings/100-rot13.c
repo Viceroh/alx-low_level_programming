8e1b396a868e67f07cf3877b87e5f44ea3a6ddea
viceroh <rotichb105@gmail.com>
2023-03-30 05:55:40 -0700

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 /**
- * rot13 - encoder rot13
- * @s: pointer to string params
- *
- * Return: *s
- */
+* rot13 - encoder rot13
+* @s: pointer to string params
+*
+* Return: *s
+*/
 
 char *rot13(char *s)
 {
